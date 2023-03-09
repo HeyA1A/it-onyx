445d668d07feb647bd49cf3c73273edcc2b49bca
HeyA1A <aauthier@ualr.edu>
2023-03-09 07:55:31 +0000

    added currency and space stuff

--- currencyvaluehistory/assets/script/js.js
+++ currencyvaluehistory/assets/script/js.js
@@ -1,84 +1,111 @@
-async function ShowResults() {
+async function Results() {
     "use strict";
-
-    var form = $("#currencyvalueForm");
-     form.validate(); 
+       
+   
+   var form = $("#currencyvalueForm");
+    
+    form.validate(); 
     
      if (form.valid()) {
+
         
         var BaseCurrency = document.getElementById("BaseCurrency").value;
         var ToCurrency = document.getElementById("ToCurrency").value;
         var FromDate = document.getElementById("FromDate").value;
         var ToDate = document.getElementById("ToDate").value;
 
-        var apiKey = "3NWUV7nTLbvIAdK6Z5W37ooGaSLBDzqYp"
+        var apiKey = "NWUV7nTLbvIAdK6Z5W37ooGaSLBDzqYp"
         
-        
-     
-      
-        var myURL1 = "https://api.polygon.io//v2/aggs/ticker/" BaseCurrency + ToCurrency + "/range/1/day/" + FromDate + "/" + ToDate + "?unadjusted=false&sort=asc&limit=32&apiKey=" + apiKey;
-        var msg2Object = await fetch(myURL1);
+        var myURL1 = "https://api.polygon.io/v3/reference/tickers/" + BaseCurrency + ToCurrency + FromDate + ToDate + "?apiKey=" + apiKey;
+        var msg1Object = await fetch(myURL1);
 
-        if (msg2Object.status >= "" && msg2Object.status <= "") {            
-            var msg1JSONText = await msg2Object.text();
+        if (msg1Object.status >= 200 && msg1Object.status <= 299) {              
+           
+            var msg1JSONText = await msg1Object.text();
           
-            var currency = [];
-            var value = [];
-            var dates = msg1.results.length;
-            if (numdays > 0) {
+            var msg1 = JSON.parse(msg1JSONText);
 
-            for (var i = 0; i < length; i++) {
+            document.getElementById("Results").innerHTML = msg1.results.name;
+        
+        }
+        else {
+         
+            alert("Currency Not Found - Status: " + msg1Object.status);
+        }        
+         
+        var myURL2 = "https://api.polygon.io//v2/aggs/ticker/" + BaseCurrency + ToCurrency + "/range/1/day/" + FromDate + "/" + ToDate + "?unadjusted=false&sort=asc&limit=32&apiKey=" + apiKey;
+           var msg2Object = await fetch(myURL2);
+    
+            if (msg2Object.status >= 200 && msg2Object.status <= 299) {            
+                var msg2JSONText = await msg2Object.text();
+           
+                var msg2 = JSON.parse(msg2JSONText);
+               
+        
+            var date = [];
+            var value = [];
+            var numdays = msg2.results.length;
+            if (numdays > 0) {
+              for (var i = 0; i < numdays; i++) {
                 
-                currency[i] = msg1.results[i].c;
-                value[i] = msg1.results[i].c;
-                var tempdate = new Date(msg1.results[i].c);
-                date[i] = tempdate.toLocaleDateString(); }}
+                value[i] = msg2.results[i].c;
+                var tempdate = new Date(msg2.results[i].t);
+                date[i] = tempdate.toLocaleDateString(); 
+            }
+        }
                
                 var ctx0 = document.getElementById("chartjs-0");
                 var myChart = new Chart(ctx0, {
                     "type":"line",
                     "data": {
-                        "labels": dates,
+                        "labels": date, 
                         "datasets":[{"label": "Currency Value History",
                         "data": value,
                         "fill":false,
                         "borderColor":"rgb(75, 192, 192)",
                         "lineTension":0.1}]},
-                        "options":{ 
-                            "options":{ 
-                                responsive: false,
-                                maintainAspectRatio: true,
+                          "options":{ 
+                             responsive: false,
+                             maintainAspectRatio: true,
                             }
-    
-    
                         }
-                    };
-            else {
+                );
+          }  
+          else {
                 alert("Currency Not Found - Status: " + msg2Object.status)
                 return
             }
         }
+    }
 
-
-
-function clearform() {
-    "use strict";
+function ClearForm() {
 
     document.getElementById("BaseCurrency").value = "";
     document.getElementById("ToCurrency").value = "";
     document.getElementById("FromDate").value = "";
     document.getElementById("ToDate").value = "";
    
-    
+ 
     var canvas0 = document.getElementById("chartjs-0");
     var context0 = canvas0.getContext('2d');    
-    context0.clearRect(0, 0, canvas0.width, canvas0.height);
+    context0.ClearRect(0, 0, canvas0.width, canvas0.height);
 
-    
-    
-    }
 }
+    
+  
+    
 
-$("#currencyvalueForm").validate({
+/*$("#currencyvalueForm").validate({
 
 });
+
+   document.getElementById("BaseCurrencyMsg").innerHTML = "";
+    document.getElementById("ToCurrencyMsg").innerHTML = "";
+    document.getElementById("FromDateMsg").innerHTML = "";
+    document.getElementById("ToDateMsg").innerHTML = "";
+
+
+ document.getElementById("Results").innerHTML = msg2JSONText; document.getElementById("Results").innerHTML = msg2JSONText;
+
+ /v2/aggs/ticker/{forexTicker}/range/{multiplier}/{timespan}/{from}/{to}
+*/
